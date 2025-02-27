23207f7b86f12231276dcc297607e20013c519d6
MarathaDada <goregaonkarnitish526@gmail.com>
2025-02-27 19:03:38 +0530

    api keys changes

--- src/js/geminiai_sript.js
+++ src/js/geminiai_sript.js
@@ -9,7 +9,7 @@ const clearChatButton = document.getElementById("deleteButton");
 let currentUserMessage = null;
 let isGeneratingResponse = false;
 
-const GOOGLE_API_KEY = "AIzaSyAEccUwH6zigKTxPTuuO9rQ5HxfMvmy0JU";
+const GOOGLE_API_KEY = "api key ";
 const API_REQUEST_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GOOGLE_API_KEY}`;
 
 // Load saved data from local storage
