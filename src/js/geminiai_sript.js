c05ad241985474e92d9720aaaff9fc9b0966f548
MarathaDada <goregaonkarnitish526@gmail.com>
2025-03-07 10:59:01 +0530

    major changes done

--- src/js/geminiai_sript.js
+++ src/js/geminiai_sript.js
@@ -1,4 +1,4 @@
-const API_KEY = ''; 
+const API_KEY = 'AIzaSyA-KnQ1XyEVLaZyKIYBV4dWzWG63M-O9gc'; 
 // Replace with your actual Gemini API key – this stores the API key to authenticate requests to the Gemini API.
 
 const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent';
